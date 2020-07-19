a5acab76659d593e41563fa84a5d0b1ae4334440
Arman Heraldo <armnjsf@gmail.com>
2020-07-18 23:21:17 -0700

    Added animation for SlowConvex

--- Computational_Geo/ConvexHull/convex.cpp
+++ Computational_Geo/ConvexHull/convex.cpp
@@ -1,5 +1,6 @@
 
 #include <vector>
+#include <string>
 #include <iostream>
 #include <unordered_map>
 #include <stdlib.h>
@@ -22,12 +23,13 @@ typedef struct Point {
 } Point;
 
 std::vector< Point > points(0);
+bool valid = false;
 
-EM_JS(void, drawPoint, (int x, int y), {
+EM_JS(void, drawPoint, (int x, int y, int r, int g, int b), {
     var ctx = document.getElementById("canvas").getContext("2d");
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, Math.PI * 2, true);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b +')';
     ctx.fill();
 });
 
@@ -41,6 +43,10 @@ EM_JS(void, drawLine, (int sx, int sy, int ex, int ey), {
     ctx.lineTo(ex, ey);
 
     ctx.stroke();
+});
+
+EM_JS(void, animateLine, (int sx, int sy, int ex, int ey), {
+    requestAnimationFrame(drawLine(sx, sy, ex, ey));
 });
 
 typedef struct hashFunc {
@@ -87,6 +93,8 @@ void slowConvex() {
             if (valid) {
                 edges[ points[i] ] = points[j];
                 drawLine(points[i].x, points[i].y, points[j].x, points[j].y);
+                //EM_ASM( requestAnimationFrame(function () {drawLine($0, $1, $2, $3);} ), points[i].x, points[i].y, points[j].x, points[j].y);
+                //drawLine(points[i].x, points[i].y, points[j].x, points[j].y);
             }
         }
     }
@@ -110,7 +118,8 @@ void randomPoints(int n) {
         int x = rand() % 480 + 10;
         int y = rand() % 480 + 10;
         addPoint(x, y);
-        drawPoint(x, y);
+        drawPoint(x, y, 255, 0, 0);
+        //EM_ASM( requestAnimationFrame(function () {drawPoint($0, $1, 255, 0, 0);} ), points[i].x, points[i].y);
     }
 }
 
@@ -118,13 +127,64 @@ void clearPoints() {
     points.clear();
 }
 
+bool getDeterminant(int i, int j, int k) {
+    int det = ( (points[k].x - points[i].x) * (points[j].y - points[i].y) - 
+                (points[k].y - points[i].y) * (points[j].x - points[i].x) );
+    
+    return det > 0;
+}
+
+std::vector<int> animateSlowConvex(int i, int j, int k) {
+    if (j == i)
+        j++;
+    if (j >= points.size()) {
+        j = 0;
+        i++;
+        return std::vector<int> { i, j, k };
+    }
+
+    if (k == 0) {
+        EM_ASM(
+            imageData = document.getElementById('canvas').getContext('2d').getImageData(0, 0, 500, 500);
+        );
+        drawLine(points[i].x, points[i].y, points[j].x, points[j].y);
+        valid = false;
+    }
+
+    valid = getDeterminant(i, j, k);
+    k++;
+    
+    if (valid) {
+        EM_ASM(
+            document.getElementById('canvas').getContext('2d').putImageData(imageData, 0, 0);
+        );
+    }
+
+    if (valid || k >= points.size()) {
+        k = 0;
+        j++;
+    }
+
+    return std::vector<int> { i, j, k };
+}
+
+std::vector< Point > getPoints() {
+    return points;
+}
+
 EMSCRIPTEN_BINDINGS(convex) {
+    emscripten::value_object< Point > ("Point")
+        .field("x", &Point::x)
+        .field("y", &Point::y);
+
+    emscripten::function("getDeterminant", &getDeterminant);
     emscripten::function("slowConvex", &slowConvex);
     emscripten::function("addPoint", &addPoint);
     emscripten::function("randomPoints", &randomPoints);
     emscripten::function("clearPoints", &clearPoints);
+    emscripten::function("getPoints", &getPoints);
+    emscripten::function("animateSlowConvex", &animateSlowConvex);
 
-    emscripten::value_object< Point > ("Point")
-        .field("x", &Point::x)
-        .field("y", &Point::y);
+    emscripten::register_vector<int>("vector<int>");
+    emscripten::register_vector<Point>("vector<Point>");
 }
